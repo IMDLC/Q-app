7a69bb31850dafcdd5c0c4c41e13400771c52737
IMDLC <125633073+IMDLC@users.noreply.github.com>
2023-05-15 19:06:16 -0600

    Add files via upload

---
+++ Js/FuncionesBtn.js
@@ -0,0 +1,11 @@
+function ir(){
+    window.location="loginEstablecimiento.html";
+}
+
+function ir2(){
+    window.location="loginClientes.html";
+}
+
+function ir3(){
+    window.location="homeClientes.html";
+}
